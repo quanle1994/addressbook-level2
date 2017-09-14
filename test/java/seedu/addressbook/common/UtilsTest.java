3bb9949f2d546cb31da498aecca2c74e2e22f59d
quanle1994 <quan_le@u.nus.edu>
2017-09-14 13:54:47 +0800

    add test for Utils.isAnyNull

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -34,6 +33,23 @@ public class UtilsTest {
         assertNotUnique(null, 1, new Integer(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
+    }
+
+    @Test
+    public void testIsAnyNull() throws Exception {
+        assertIsAllNull(null,null,null,null);
+        assertIsAllNull(null,null,null,"abc");
+        assertIsAllNull(null,null,"abc","Aaa");
+        assertIsAllNull(null,"def","abc","aaa");
+        assertIsAllNotNull(new Integer(123),"def","abc","aaa");
+    }
+
+    private void assertIsAllNotNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+
+    private void assertIsAllNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
     }
 
     private void assertAreUnique(Object... objects) {
